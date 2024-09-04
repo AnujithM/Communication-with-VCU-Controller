9fda9214942d264e218b86023cb38c3bf54b480a
Anujith Muraleedharan <140189296+AnujithM@users.noreply.github.com>
2024-09-04 20:47:31 +0530

    commit

--- Files Part (b) & (c)/src/vehicle_data_publisher.py
+++ Files Part (b) & (c)/src/vehicle_data_publisher.py
@@ -6,19 +6,19 @@ import random
 def publish_data():
     rospy.init_node('vehicle_data_publisher', anonymous=True)
     pub = rospy.Publisher('vehicle_data', Int32MultiArray, queue_size=10)
-    rate = rospy.Rate(10)  # 10 Hz corresponds to 100ms
+    rate = rospy.Rate(10)  
 
     while not rospy.is_shutdown():
         data = Int32MultiArray()
         data.data = [
-            random.randint(0, 255),  # X Position
-            random.randint(0, 255),  # Y Position
-            random.randint(0, 255),  # X Linear Velocity
-            random.randint(0, 255),  # Y Linear Velocity
-            random.randint(0, 255),  # X Angular Velocity
-            random.randint(0, 255),  # Y Angular Velocity
-            random.randint(0, 255),  # Random Number 1
-            random.randint(0, 255)   # Random Number 2
+            random.randint(0, 255),  
+            random.randint(0, 255),  
+            random.randint(0, 255),  
+            random.randint(0, 255),  
+            random.randint(0, 255),  
+            random.randint(0, 255),  
+            random.randint(0, 255), 
+            random.randint(0, 255)   
         ]
         pub.publish(data)
         rate.sleep()
